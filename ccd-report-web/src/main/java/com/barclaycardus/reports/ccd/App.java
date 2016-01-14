3bd025089b4a41fe619df3a15fe4123f57637894
Abhishek Biswas <biswas.abhishek@ymail.com>
2016-01-15 02:20:31 +0530

    ccd-report: setting up basic project structure

---
+++ ccd-report-web/src/main/java/com/barclaycardus/reports/ccd/App.java
@@ -0,0 +1,13 @@
+package com.barclaycardus.reports.ccd;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
